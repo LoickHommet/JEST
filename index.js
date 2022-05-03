e95b348f4a117170828e634cd591f9de85601c86
Loick Hommet <hommet.loick@gmail.com>
2022-05-03 16:46:25 +0200

    reacto avec la fonction math

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 module.exports = function absolue(a) {
-    // vérifier que l'agument est un nombre
     // retourne la valeur absolue
-    if(typeof a === 'number') // !isNaN(a)
-    {
-        if (a === 0) return 0;
-        else if (a < 0) return -a;
-        else return a;
-    }else{
-        throw new Error('Sa doit etre un nombre')
-    }
+    if(typeof a !== 'number')  throw new Error('Sa doit etre un nombre')// !isNaN(a)
+    return Math.abs(a)
+    
 };
