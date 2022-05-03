1b1bdb365e3c38f3ead39bde8827ea99f570c5a5
Loick Hommet <hommet.loick@gmail.com>
2022-05-03 16:41:55 +0200

    condition si a est un nombre

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 module.exports = function absolue(a) {
     // vérifier que l'agument est un nombre
     // retourne la valeur absolue
-
+    if(typeof a === 'number') // !isNaN(a)
+    {
         if (a === 0) return 0;
         else if (a < 0) return -a;
         else return a;
-  
+    }else{
+        throw new Error('Sa doit etre un nombre')
+    }
 };
