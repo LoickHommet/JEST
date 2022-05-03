12f8de30270e762caeff3e8afd3a1ae9b4dd5536
Loick Hommet <hommet.loick@gmail.com>
2022-05-03 16:52:37 +0200

    Ajoue de decribe pour groupe les teste et présentation de fonction de Préparation et Nettoyage

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,41 +1,48 @@
 const { boolean } = require("yargs");
 const absolue = require("../index");
 
-test("absolue - doit retourner 0 si on lui passe 0", () => {
-  const result = absolue(0);
-  expect(result).toBe(0);
-});
+describe("La fonction absolue", function () {
 
-test("absolue - doit retourner 1 si on lui passe -1", () => {
-  const result = absolue(-1);
-  expect(result).toBe(1);
-});
+    beforeEach(()=>{})
+    afterEach(()=>{})
+    beforeAll(()=>{})
+    afterAll(()=>{})
 
-test("absolue - doit retourner 1 si on lui passe 1", () => {
-  const result = absolue(1);
-  expect(result).toBe(1);
-});
+  test("absolue - doit retourner 0 si on lui passe 0", () => {
+    const result = absolue(0);
+    expect(result).toBe(0);
+  });
 
-test('absolue - doit retourner infinity si on lui passe infinity', () =>{
-    const result = absolue(Infinity)
-    expect(result).toBe(Infinity)
-});
-test('absolue - doit retourner infinity si on lui passe -infinity', () =>{
-    const result = absolue(-Infinity)
-    expect(result).toBe(Infinity)
-});
+  test("absolue - doit retourner 1 si on lui passe -1", () => {
+    const result = absolue(-1);
+    expect(result).toBe(1);
+  });
 
+  test("absolue - doit retourner 1 si on lui passe 1", () => {
+    const result = absolue(1);
+    expect(result).toBe(1);
+  });
 
-test('absolue - doit retourner NaN si on lui passe NaN', () =>{
-    const result = absolue(NaN)
-    expect(result).toBe(NaN)
-});
+  test("absolue - doit retourner infinity si on lui passe infinity", () => {
+    const result = absolue(Infinity);
+    expect(result).toBe(Infinity);
+  });
+  test("absolue - doit retourner infinity si on lui passe -infinity", () => {
+    const result = absolue(-Infinity);
+    expect(result).toBe(Infinity);
+  });
 
-test.each([undefined, 'zzz', '', ' ', [],[1], {}, {id:1}, boolean])(
-    "absolue - doit rejter ce qui n'est pas un nombre, ici %p", (a) =>{
-        expect(() => {absolue(a)}).toThrow(/nombre/);
+  test("absolue - doit retourner NaN si on lui passe NaN", () => {
+    const result = absolue(NaN);
+    expect(result).toBe(NaN);
+  });
 
+  test.each([undefined, "zzz", "", " ", [], [1], {}, { id: 1 }, boolean])(
+    "absolue - doit rejter ce qui n'est pas un nombre, ici %p",
+    (a) => {
+      expect(() => {
+        absolue(a);
+      }).toThrow(/nombre/);
     }
-)
-
-
+  );
+});
